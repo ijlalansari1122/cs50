d344516f95c32045a83a67134cbd6ff5df6d083f
ijlalansari1122 <ansariijlal90@gmail.com>
2024-07-02 16:35:14 +0500

    conditionals have been added

---
+++ C-tasks/Conditionals.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+int main(){
+    
+
+int num1 = 23;
+
+int num2 = 45;
+
+
+if (num1 < num2)
+{
+    
+   printf("yes  num1 is less then num2");
+}
+else if (num2 >num1)
+{
+
+   printf("No this num1 is greater then num2");
+
+}
+else 
+{
+
+printf("num1 is equal to num2");    
+
+}
+
+
+
+}
---
+++ C-tasks/tasks/Type.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char chr(){
+
+char name ="ijlal";
+
+
+}
+
+void main(){
+
+int num = 23;
+
+float perc = 233.33;
+
+
+
+printf("the float num is %f",perc );
+printf("the int is %d",num);
+printf("the name  is ",chr());
+
+}
