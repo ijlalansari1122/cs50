fa7b270e6b3618220d1c3c5770267186a49dfbe9
ijlalansari1122 <ansariijlal90@gmail.com>
2024-07-04 10:34:28 +0500

    wsl has been used in vs code  for cs50 course

---
+++ C-tasks/Con.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(){
+    
+
+int x =get_int("what is x")
+
+int y =get_int("what is y")
+ 
+
+
+if (x < y)
+{
+    
+   printf("yes  x is less then y");
+}
+else if (num2 >num1)
+{
+
+   printf("No this x is greater then y");
+
+}
+else 
+{
+
+printf("num1 is equal to num2");    
+
+}
+
+
+
+}
